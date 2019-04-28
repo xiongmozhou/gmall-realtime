260936034969222b1c280b21615a051037c9b2fb
xmz <1250398105@qq.com>
2019-04-28 22:34:49 +0800

    second commint day01

---
+++ dw-common/src/main/java/com/atguigu/gmall1111/common/constant/GmallConstant.java
@@ -0,0 +1,6 @@
+package com.atguigu.gmall1111.common.constant;
+
+public class GmallConstant {
+    public static final String KAFKA_TOPIC_STARTUP = "GMALL_STARTUP";
+    public static final String KAFKA_TOPIC_EVENT = "GMALL_EVENT";
+}
