50a7e89598fdc6906de746134caa889cf3c3de0d
xmz <1250398105@qq.com>
2019-04-28 22:30:21 +0800

    second commint day01

--- dw-mock/src/main/java/com/atguigu/gmall/dw/mock/JsonMocker.java
+++ dw-mock/src/main/java/com/atguigu/gmall/dw/mock/JsonMocker.java
@@ -3,7 +3,6 @@ package com.atguigu.gmall.dw.mock;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.gmall.dw.mock.utils.*;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
--- dw-mock/src/main/java/com/atguigu/gmall/dw/mock/utils/LogUploader.java
+++ dw-mock/src/main/java/com/atguigu/gmall/dw/mock/utils/LogUploader.java
@@ -1,38 +1,16 @@
 package com.atguigu.gmall.dw.mock.utils;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-
-import java.io.File;
-import java.io.IOException;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class LogUploader {
-
-
-
 
     public static void sendLogStream(String log){
         try{
             //不同的日志类型对应不同的URL
 
-            URL url  =new URL("http://logserver/log");
+            URL url  =new URL("http://hadoop102:8080/log");
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             //设置请求方式为post
